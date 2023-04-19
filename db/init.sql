f5845bf74034fb6ee85a2ffeef69743ae3af3814
Shirley <115416417+sslo12@users.noreply.github.com>
2023-04-19 00:55:04 -0500

    Add files via upload

---
+++ db/init.sql
@@ -0,0 +1,32 @@
+create database almacen;
+use almacen;
+
+create table usuarios (
+    nombre varchar(50),
+    email varchar(50),
+    usuario varchar(20),
+    password varchar(20),
+    primary key (usuario)
+);
+
+create table productos (
+    id int auto_increment,
+    nombre varchar(50),
+    precio decimal(10,2),
+    inventario int,
+    primary key(id)
+);
+
+create table ordenes (
+    id int auto_increment,
+    nombreCliente varchar(50),
+    emailCliente varchar(50),
+    totalCuenta decimal(10,2),
+    fecha datetime default current_timestamp,
+    primary key(id)
+);
+
+INSERT INTO usuarios
+    (nombre, email, usuario, password)
+VALUES
+    ('shirley', 'shirley@gmail.com','admin','shir')
